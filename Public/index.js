08571e665570eee22691a204d608c32e5c30bb48
user <will2012.tseng@gmail.com>
2025-12-17 11:23:02 +0800

    added new feature

--- Public/index.js
+++ Public/index.js
@@ -1,1 +1,15 @@
-alert("Hello")
+// Ask how many times to spam
+let times = prompt("How many times do you want to spam the message?");
+times = Number(times);
+
+// Ask for the message
+let message = prompt("Enter the message to spam");
+
+// Check input and spam
+if (Number.isInteger(times) && times > 0) {
+    for (let i = 1; i <= times; i++) {
+        console.log(i + ": " + message);
+    }
+} else {
+    alert("Please enter a valid positive number.");
+}
